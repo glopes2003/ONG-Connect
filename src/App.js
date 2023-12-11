6b66a5ffd29a6e7dfbead6c8f347bfb63836f4e4
Giovana <gicaalopes2003@gmail.com>
2023-12-10 22:05:43 -0300

    file responsible for rendering code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import './index.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Login from "./pages/Login"
+import Register from "./pages/Register"
+import Query from "./pages/Query";
+import Options from "./pages/Options";
+import Results from './pages/Results';
+
+const App = () => {
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <>
+    <Router>
+        <Routes>
+          <Route path="/" element={<Login/>} />
+          <Route path="/register" element={<Register title="Registro" />} />
+          <Route path="/query"  element={<Query title="Faça a sua consulta" />} />
+          <Route path="/results"  element={<Results title="Resultados" />} />
+          <Route path="/options"  element={<Options />} />
+        </Routes>
+      </Router>
+    </>
+  </React.StrictMode>,
+)
+
 }
 
-export default App;
+export default App; 
